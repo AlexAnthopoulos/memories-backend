bc73ebdf0a925270205d35edaba29ed31edae3c2
Alex Anthopoulos <alexanthopoulos@Alexs-iMac.local>
2020-09-16 13:36:21 +0200

    added models for user and memories

---
+++ models/Memories.model.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const Memory = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    comments: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+    },
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+    memoryDate: {
+      required: true,
+      type: Date,
+    },
+    user: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: true,
+  }
+);
