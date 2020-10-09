2acfed1c82e837855dfeb58b7c4b1fee69a8c208
AlexAnthopoulos <alex.anthopoulos01@gmail.com>
2020-10-09 12:49:36 +0200

    logs removal

--- api/logs.js
+++ api/logs.js
@@ -4,8 +4,6 @@ const router = Router();
 const Memory = require("../models/Memories.model");
 
 router.get("/logs", (req, res, next) => {
-  //console.log("Logs get: The body is: ", req.body);
-  console.log("Logs get: The user is ", req.user);
   Memory.find()
     .then((results) => {
       res.send(results);
@@ -13,17 +11,9 @@ router.get("/logs", (req, res, next) => {
     .catch((error) => {
       next(error);
     });
-  // try {
-  //   const entries = await Memory.find();
-  //   res.json(entries);
-  // } catch (error) {
-  //   next(error);
-  // }
 });
 
 router.get("/userlogs", (req, res, next) => {
-  //console.log("Logs get: The body is: ", req.body);
-  console.log("Logs get: The user is ", req.user);
   Memory.find({
     user: mongoose.Types.ObjectId(req.user._id),
   })
@@ -37,8 +27,7 @@ router.get("/userlogs", (req, res, next) => {
 
 router.delete("/userlogs/:id", (req, res, next) => {
   // req.params.id
-  console.log("Logs delete:", req.params.id);
-  console.log("Logs get: The user is ", req.user);
+
   Memory.findByIdAndDelete(req.params.id)
     .then((results) => {
       res.send(results);
@@ -49,9 +38,6 @@ router.delete("/userlogs/:id", (req, res, next) => {
 });
 
 router.post("/logs", (req, res, next) => {
-  console.log("Logs : The body is Memories: ", req.body);
-  console.log("Logs: The user is ", req.user);
-  console.log("This is the request ", req);
   const newMemory = {
     ...req.body,
     user: mongoose.Types.ObjectId(req.user._id),
@@ -63,16 +49,5 @@ router.post("/logs", (req, res, next) => {
     .catch((error) => {
       next(error);
     });
-  // try {
-  //   const memory = new Memory(req.body);
-  //   const createdMemory = await memory.save();
-  //   res.json(createdMemory);
-  // } catch (error) {
-  //   console.log(error.name);
-  //   if (error.name === "Validation Error") {
-  //     res.status(422);
-  //   }
-  //   next(error);
-  // }
 });
 module.exports = router;
