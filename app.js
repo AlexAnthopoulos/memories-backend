4007ac5c424fe473de7a46e2d4ec95c8483a7316
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-24 22:08:28 +0200

    :recycle: Cleaned up app.js by renoving unused imports

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 require("dotenv").config();
-const logs = require("./api/logs");
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const favicon = require("serve-favicon");
-const hbs = require("hbs");
-const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
-const User = require("./models/User.model");
 const session = require("express-session");
 const passport = require("passport");
 const cors = require("cors");
-require("./configs/passport");
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
   `${app_name}:${path.basename(__filename).split(".")[0]}`
 );
 
-const app = express();
+// require passport configs
+require("./configs/passport");
 
 // require database configuration
 require("./configs/db.config");
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.FRONTEND_POINT,
-  })
-);
+
+const app = express();
+
+// Express View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // Middleware Setup
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Express View engine setup
-
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_POINT,
+  })
+);
 app.use(
   require("node-sass-middleware")({
     src: path.join(__dirname, "public"),
@@ -46,13 +47,7 @@ app.use(
     sourceMap: true,
   })
 );
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
-
-//Add Sesion settings here :
+//Add Session settings here :
 app.use(
   session({
     secret: process.env.SESSION_KEY,
@@ -62,18 +57,16 @@ app.use(
 );
 
 //Passport initialize and passport session here:
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 // default value for title local
 app.locals.title = "Express - Generated";
 
-const index = require("./routes/index");
-app.use("/", index);
-const authRoutes = require("./routes/auth-routes");
-app.use("/api", authRoutes);
+// Routes setup
+app.use("/", require("./routes/index"));
+app.use("/api", require("./routes/auth-routes"));
 app.use("/api", require("./routes/file-upload-routes"));
-app.use("/api", logs);
+app.use("/api", require("./api/logs"));
 
 module.exports = app;
